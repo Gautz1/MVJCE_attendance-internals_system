debeb8d242218bccf966606e7abcbc70540d5209
narein97 <jabbawock003@gmail.com>
2017-04-29 19:26:29 +0530

    Add files via upload

---
+++ mvjce/Mvjce.java
@@ -0,0 +1,18 @@
+package mvjce;
+
+/**
+ *
+ * @author gautamdhariharan
+ */
+public class Mvjce {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        loginScreen ls=new loginScreen();
+        ls.setVisible(true);
+    }
+    
+}
+
